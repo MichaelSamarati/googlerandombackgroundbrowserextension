2ef3bab943b39f8e80f785275baa7ad95a421b6f
MiritoSun <87271458+MichaelSamarati@users.noreply.github.com>
2023-02-06 22:51:50 +0100

    update

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ run();
 
 function run() {
   const mainElement = document.getElementsByClassName("L3eUgb")[0];
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("random-background-image-wrapper");
-  newDiv.classList.add("no-select");
-  newDiv.innerHTML =
-    "<div class='random-background-image random-background-image-blur'></div>" + //random-background-image-blur is used in blur chrome extension
-    "<div class='random-background-image random-background-image-normal'></div>"; //random-background-image-normal is used in blur chrome extension
-  mainElement.appendChild(newDiv);
+  const randomBackgroundImageWrapper = document.createElement("div");
+  randomBackgroundImageWrapper.classList.add("random-background-image-wrapper");
+  randomBackgroundImageWrapper.classList.add("no-select");
+  randomBackgroundImageWrapper.innerHTML =
+    "<div class='random-background-image'></div>";
+  mainElement.appendChild(randomBackgroundImageWrapper);
 }
